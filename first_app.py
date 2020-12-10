2be844e29dd2bf5daeb0339c30b82b6b51034de1
ries9112 <ries9112@colorado.edu>
2020-12-09 21:40:29 -0700

    finalize simple example

--- first_app.py
+++ first_app.py
@@ -1,25 +1,41 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import pins
+import plotly.express as px
 
+# Pull data
+@st.cache
+def load_data():
+    df = pins.pin_get("hitBTC_orderbook", board = "https://raw.githubusercontent.com/predictcrypto/pins/master/")
+    return df
+# Pull data
+df = load_data()
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+# Sidebar user input
+crypto_symbol = st.sidebar.selectbox(
+    'Which cryptocurrency do you want to visualize?',
+     df['symbol'])
+# Show the data
+st.write("Preview of the latest data (all cryptocurrencies):")
+# Move date time to the front
+df = df[ ['date_time_utc'] + [ col for col in df.columns if col != 'date_time_utc' ] ]
+# Arrange by date time
+df.sort_values(by=['date_time_utc'])
+# Show data
+st.write(df.head(250))
 
+# Show selected cryptocurrency
+'Data for the ', crypto_symbol, 'cryptocurrency:'
+# Display data
+st.write(df[(df.symbol == crypto_symbol)])
+# Add note about changing selection
+st.write('Use the sidebar on the left to change the selected cryptocurrency')
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+# Plot the data
+state_total_graph = px.line(
+        df[(df.symbol == crypto_symbol)], 
+        x='date_time_utc',
+        y='ask_1_price')
 
-st.line_chart(chart_data)
-
-
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
-
+st.plotly_chart(state_total_graph)
